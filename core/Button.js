274b261bee0e88c7de8159490421b7712282e344
Bratfisch <sgt-bratfisch@guetersloh.gt-net.de>
2022-04-20 11:43:13 +0200

    Test Commit

---
+++ core/Button.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  /* Adapt the colors based on primary prop */
+  background: "white"};
+  color: "palevioletred"};
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+
+export default Button;
